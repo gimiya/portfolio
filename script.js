6786753c45b0955309df97029659c0f270018d2c
gimiya <68461092+gimiya@users.noreply.github.com>
2020-11-28 17:58:23 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,56 @@
+$(function() {
+
+  /* 開閉ボタンをクリックしたら以下の関数を実行 */
+  $('#hamburger-nav-toggle').click(function() {
+  
+  /* 開閉ボタンのクラスが'close'（閉じている状態）の場合 */
+  if($('#hamburger-nav-toggle').attr("class") == 'hamburger-nav-toggle-btn close') {
+  
+    /* ハンバーガーメニューを展開 */
+    $('.hamburger-nav-list').slideDown();
+  
+        /* さらに、開閉ボタンのクラスから'close'を削除し、'open'を付与する */
+    $(this).removeClass("close").addClass("open");
+  }
+  /* 開閉ボタンのクラスが'close'ではない場合 */
+  else {
+  /* ハンバーガーメニューを収束 */
+    $('.hamburger-nav-list').slideUp(function() {
+        /* メニューが収束したのち、開閉ボタンのクラスから'open'を削除し、'close'を付与する */
+        $('#hamburger-nav-toggle').removeClass("open").addClass("close");
+      });
+    }
+  }); /* 開閉ボタンをクリックした時の動作はここまで */
+  
+    /* ハンバーガーメニューのアンカーをクリックしたら以下の関数を実行 */
+    $('.hamburger-nav-list a').click(function() {
+  
+      /* 開いている必要はないので、ハンバーガーメニューを収束 */
+      $('.hamburger-nav-list').slideUp(function() {
+  
+        /* メニューが収束したのち、開閉ボタンのクラスから'open'を削除し、'close'を付与する */
+      $('#hamburger-nav-toggle').removeClass("open").addClass("close");
+    });
+  });
+  
+  /* SmoothScroll（おまけ） */
+  // $('.hamburger-nav-list a').smoothScroll();
+});
+
+jQuery(function() {
+  var pagetop = $('#page_top');   
+  pagetop.hide();
+  $(window).scroll(function () {
+      if ($(this).scrollTop() > 100) {  //100pxスクロールしたら表示
+          pagetop.fadeIn();
+      } else {
+          pagetop.fadeOut();
+      }
+  });
+  pagetop.click(function () {
+      $('body,html').animate({
+          scrollTop: 0
+      }, 500); //0.5秒かけてトップへ移動
+      return false;
+  });
+});
